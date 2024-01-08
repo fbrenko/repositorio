724bab901635bcd415eefaedfa7f9b20198651b9
Fernando Brenko <fernandobrenko@gmail.com>
2024-01-08 18:10:08 -0300

    Maestria Ciencia de Datos - Prueba

---
+++ fff.py
@@ -0,0 +1,11 @@
+import pandas as pd # importo pandas para el tratamiento de data frames
+import numpy as np  # import numpy para el manejo de arrays
+from sklearn import linear_model # SKLEARN es la libreria de Machine Learning que implementa el algoritmo de Regresión Lineal
+
+def new_func():
+    edad = 25
+
+    if (edad>20):
+        print('fer')
+
+new_func()
